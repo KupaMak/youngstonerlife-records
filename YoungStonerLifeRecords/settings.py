48897f75a5190a37864a3875dd4a6ae35976ee7b
Kupa <kupsamos@gmail.com>
2022-09-17 10:49:18 +0200

    changes2

--- YoungStonerLifeRecords/settings.py
+++ YoungStonerLifeRecords/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mwuj03#5vmto47c9c9elep=!4ol#woy!zy%*j8z!0r6-m29%@p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['youngstonerlife.herokuapp.com','127.0.0.1']
 
 # Application definition
 
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'user',
+    'whitenoise.runserver_nostatic'
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,16 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "static"
+
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
 
-    BASE_DIR / 'static'
-
-]
 
 MEDIA_URL = 'uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
